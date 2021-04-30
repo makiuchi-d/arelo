114027aeb2deace7c9c4a32c4703f859eaba0edd
makki_d <d.makiuchi@gmail.com>
2021-04-30 23:52:55 +0900

    Send SIGCONT after sending terminate signal.
    see: https://junkyard.song.mu/slides/gocon2019-spring/#52

--- cmd_unix.go
+++ cmd_unix.go
@@ -14,5 +14,10 @@ func prepareCommand(cmd []string) *exec.Cmd {
 }
 
 func killChilds(c *exec.Cmd, sig syscall.Signal) error {
-	return syscall.Kill(-c.Process.Pid, sig)
+	err := syscall.Kill(-c.Process.Pid, sig)
+	if err == nil && sig != syscall.SIGKILL && sig != syscall.SIGCONT {
+		// prosess can be stopped, so it must be start by SIGCONT.
+		err = syscall.Kill(-c.Process.Pid, syscall.SIGCONT)
+	}
+	return err
 }
