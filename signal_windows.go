b50191002d432f0864e92738b0f63fa15e19985b
makki_d <d.makiuchi@gmail.com>
2021-06-26 01:13:31 +0900

    fix parseSignalOption for windows

--- signal_windows.go
+++ signal_windows.go
@@ -4,8 +4,12 @@ package main
 
 import (
 	"os"
+	"syscall"
 )
 
 func parseSignalOption(str string) (os.Signal, string) {
+	if str == "SIGTERM" {
+		return syscall.SIGTERM, ""
+	}
 	return nil, "Signal option (--signal, -s) is not available on Windows."
 }
